fb3a7e26fc951c575258464071dd50b1213863cf
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2023-11-15 14:41:38 +0100

    Initial commit

---
+++ apps/src/FunctionOne/src/FunctionOne/SqsHandler.cs
@@ -0,0 +1,20 @@
+using Amazon.Lambda.Annotations;
+using Amazon.Lambda.Annotations.APIGateway;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
+using Microsoft.Extensions.Configuration;
+
+namespace FunctionOne;
+
+public class SqsHandler
+{
+    [LambdaFunction]
+    public string Handler(SQSEvent sqsEvent, [FromServices] IConfiguration configuration, ILambdaContext context)
+    {
+        foreach (var message in sqsEvent.Records)
+        {
+            context.Logger.LogInformation($"Processed message {message.Body}");
+        }
+        return configuration["HelloKey"];
+    }
+}
